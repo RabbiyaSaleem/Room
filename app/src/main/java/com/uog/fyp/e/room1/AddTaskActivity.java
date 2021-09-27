089073bd9e701d1c22bacf2345908e81311a074a
Rabbiya saleem <rabisheikh1357@gmail.com>
2021-09-27 12:16:55 +0500

    gitadd

---
+++ app/src/main/java/com/uog/fyp/e/room1/AddTaskActivity.java
@@ -0,0 +1,88 @@
+package com.uog.fyp.e.room1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.uog.fyp.e.room1.Room.DatabaseClient;
+import com.uog.fyp.e.room1.Room.Task;
+
+public class AddTaskActivity extends Activity {
+    private EditText editTextTask, editTextDesc, editTextFinishBy;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addtask);
+
+        editTextTask = findViewById(R.id.editTextTask);
+        editTextDesc = findViewById(R.id.editTextDesc);
+        editTextFinishBy = findViewById(R.id.editTextFinishBy);
+
+        findViewById(R.id.button_save).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveTask();
+            }
+        });
+    }
+
+    private void saveTask() {
+        final String sTask = editTextTask.getText().toString().trim();
+        final String sDesc = editTextDesc.getText().toString().trim();
+        final String sFinishBy = editTextFinishBy.getText().toString().trim();
+
+        if (sTask.isEmpty()) {
+            editTextTask.setError("Task required");
+            editTextTask.requestFocus();
+            return;
+        }
+
+        if (sDesc.isEmpty()) {
+            editTextDesc.setError("Desc required");
+            editTextDesc.requestFocus();
+            return;
+        }
+
+        if (sFinishBy.isEmpty()) {
+            editTextFinishBy.setError("Finish by required");
+            editTextFinishBy.requestFocus();
+            return;
+        }
+
+        class SaveTask extends AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+                //creating a task
+                Task task = new Task();
+                task.setTask(sTask);
+                task.setDesc(sDesc);
+                task.setFinishBy(sFinishBy);
+                task.setFinished(false);
+
+                //adding to database
+                DatabaseClient.getInstance(getApplicationContext()).getAppDatabase()
+                        .taskDao()
+                        .insert(task);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                finish();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        SaveTask st = new SaveTask();
+        st.execute();
+    }
+}
